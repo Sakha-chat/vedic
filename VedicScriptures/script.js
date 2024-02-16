7b7f7ffb63f64ee4acbd4eeed974a4d35aa36875
sahil-m04 <sahilmehta2703@gmail.com>
2024-02-16 22:30:46 +0530

    Added Necessary Files

---
+++ VedicScriptures/script.js
@@ -0,0 +1,67 @@
+const expand_btn = document.querySelector(".expand-btn");
+
+let activeIndex;
+
+expand_btn.addEventListener("click", () => {
+  document.body.classList.toggle("collapsed");
+});
+
+const current = window.location.href;
+
+const allLinks = document.querySelectorAll(".sidebar-links a");
+
+allLinks.forEach((elem) => {
+  elem.addEventListener("click", function () {
+    const hrefLinkClick = elem.href;
+
+    allLinks.forEach((link) => {
+      if (link.href == hrefLinkClick) {
+        link.classList.add("active");
+      } else {
+        link.classList.remove("active");
+      }
+    });
+  });
+});
+
+const searchInput = document.querySelector(".search__wrapper input");
+
+searchInput.addEventListener("focus", (e) => {
+  document.body.classList.remove("collapsed");
+});
+$(document).ready(function () {
+  console.log("Document ready");
+  $(".expand-btn").click(function () {
+    console.log("Expand button clicked");
+    $(".sidebar-links").toggleClass("active");
+  });
+});
+function myFunction() {
+  let element = document.body;
+  element.classList.toggle("light-mode");
+  if (element.classList.contains("light-mode")) {
+    icon.src = "/Assets/Light-theme-icon/moon.png";
+  } else {
+    icon.src = "/Assets/Light-theme-icon/sun.png"
+  }
+}
+function displayMessage() {
+  const message = document.createElement("div");
+  if (document.body.classList.contains("light-mode")) {
+    message.textContent = "Switch to dark mode";
+  } else {
+    message.textContent = "Switch to light mode";
+  }
+  message.classList.add("hover-message");
+  document.body.appendChild(message);
+}
+
+
+function hideMessage() {
+  const message = document.querySelector(".hover-message");
+  if (message) {
+    message.remove();
+  }
+}
+
+
